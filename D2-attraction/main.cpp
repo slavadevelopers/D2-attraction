1686e23a39fa2aa0ccacf35a0c6e399fcc39f4ed
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-10-19 22:24:32 +0300

    Test D2 completed

---
+++ D2-attraction/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short int x;
+    cin >> x;
+    if (x >= 150) {
+        cout << 1 << endl;
+        return 0;
+    }
+    cout << 0 << endl;
+    return 0;
+}
